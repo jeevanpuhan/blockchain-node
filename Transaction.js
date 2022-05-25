6954206dd1243bd355ba51f701aa3834052cc3ab
Jeevan Puhan <jeevankumarpuhan@gmail.com>
2022-05-25 16:46:48 +0530

    introduce signing key

--- Transaction.js
+++ Transaction.js
@@ -1,9 +1,16 @@
+const SHA256 = require("crypto-js/sha256");
 class Transaction {
 	constructor(fromAddress, toAddress, amount) {
 		this.fromAddress = fromAddress;
 		this.toAddress = toAddress;
 		this.amount = amount;
 	}
+
+	calculateHash() {
+		return SHA256(this.fromAddress + this.toAddress + this.amount).toString();
+	}
+
+	signTransaction(signingKey) {}
 }
 
 module.exports.Transaction = Transaction;
