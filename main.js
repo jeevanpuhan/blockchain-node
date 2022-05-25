a91bb54b4d9fb9cc3ecc865e88b19924d53182ca
Jeevan Puhan <jeevankumarpuhan@gmail.com>
2022-05-25 12:24:04 +0530

    check validity of blockchain

--- main.js
+++ main.js
@@ -37,10 +37,38 @@ class Blockchain {
 		newBlock.hash = newBlock.calculateHash();
 		this.chain.push(newBlock);
 	}
+
+	isChainValid() {
+		for (let i = 1; i < this.chain.length; i++) {
+			const currentBlock = this.chain[i];
+			const previousBlock = this.chain[i - 1];
+
+			// if hash of the block is still valid
+			if (currentBlock.hash != currentBlock.calculateHash()) {
+				return false;
+			}
+
+			// if our block points to a correct previous block
+			// to check if the previous hash property is correctly set
+			if (currentBlock.previousHash !== previousBlock.hash) {
+				return false;
+			}
+		}
+
+		return true;
+	}
 }
 
 let bitcoin = new Blockchain();
 bitcoin.addBlock(new Block(1, "26/05/2022", { amount: 4 }));
 bitcoin.addBlock(new Block(2, "27/05/2022", { amount: 10 }));
 
-console.log(JSON.stringify(bitcoin, null, 4));
+console.log("Is blockchain valid?", bitcoin.isChainValid());
+
+bitcoin.chain[1].data = { amount: 100 };
+// tamper with block
+bitcoin.chain[1].hash = bitcoin.chain[1].calculateHash();
+
+console.log("Is blockchain valid after change?", bitcoin.isChainValid());
+
+// console.log(JSON.stringify(bitcoin, null, 4));
